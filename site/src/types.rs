a31addbe17af717f5bdf3e99629af9c9be94ce93
sem pruijs <sem@pruijs.net>
2025-06-28 23:20:25 +0200

    write sidebar

--- site/src/types.rs
+++ site/src/types.rs
@@ -18,6 +18,20 @@ pub struct Chapter {
     pub verses: Vec<Verse>,
 }
 
+impl Chapter {
+    pub fn to_path(&self) -> String {
+        // Trim off the final " <number>" part to get the book name
+        let mut name = self.name.trim_end().to_string();
+
+        if let Some(pos) = name.rfind(' ') {
+            name.truncate(pos); // remove everything after the last space
+        }
+
+        let book = name.replace(' ', "_");
+        format!("{}/{}", book, self.chapter)
+    }
+}
+
 #[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct Verse {
     pub verse: u32,
