d8f2878bbacd7489ce99f7ed7c1e032a1b004cea
sem pruijs <sem@pruijs.net>
2025-06-29 00:21:33 +0200

    move reading chapter from url outside chapterView

--- site/src/chapter_view.rs
+++ site/src/chapter_view.rs
@@ -5,26 +5,7 @@ use leptos_router::hooks::use_params_map;
 use crate::types::*;
 
 #[component]
-pub fn ChapterView(bible: Bible) -> impl IntoView {
-    let params = use_params_map();
-    let book = move || params.read().get("book").unwrap();
-    let chapter = move || {
-        params
-            .read()
-            .get("chapter")
-            .and_then(|s| s.parse::<u32>().ok())
-            .unwrap_or(1) // fallback chapter number if parsing fails
-    };
-
-    let chapter: Chapter = bible.get_chapter(&book(), chapter()).unwrap();
-
-    view! {
-        <ChapterDetail chapter=chapter />
-    }
-}
-
-#[component]
-fn ChapterDetail(chapter: Chapter) -> impl IntoView  {
+pub fn ChapterDetail(chapter: Chapter) -> impl IntoView  {
     view! {
         <h1>{chapter.name.as_str()}</h1>
         {chapter.verses.iter().map(|verse| {
