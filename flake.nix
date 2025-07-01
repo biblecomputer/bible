4cd121d726b9e2da1f55144171d34be615b26d72
sem pruijs <sem@pruijs.net>
2025-07-01 22:43:48 +0200

    write flake that builds static website

--- flake.nix
+++ flake.nix
@@ -1,37 +1,125 @@
 {
-  description = "Rust flake using naersk and rust-overlay";
+  description = "Build a cargo project with Trunk and Nix";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     flake-utils.url = "github:numtide/flake-utils";
-    naersk.url = "github:nix-community/naersk";
-    rust-overlay.url = "github:oxalica/rust-overlay";
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    crane.url = "github:ipetkov/crane";
   };
 
-  outputs = { self, nixpkgs, flake-utils, naersk, rust-overlay }:
+  outputs = { self, nixpkgs, flake-utils, crane, rust-overlay, ... }:
     flake-utils.lib.eachDefaultSystem (system:
       let
-        overlays = [ rust-overlay.overlays.default ];
         pkgs = import nixpkgs {
-          inherit system overlays;
+          inherit system;
+          overlays = [ (import rust-overlay) ];
         };
 
-        rust-toolchain = pkgs.rust-bin.stable.latest.default.override {
+        rustToolchain = pkgs.rust-bin.stable.latest.default.override {
           targets = [ "wasm32-unknown-unknown" ];
         };
-        naersk-lib = pkgs.callPackage naersk { inherit rust-toolchain; };
-      in {
-        packages.default = naersk-lib.buildPackage {
-          pname = "leptos-tutorial";
-          root = ./leptos-tutorial;
-        };
 
-        devShells.default = pkgs.mkShell {
-          packages = [
-            rust-toolchain
-            pkgs.rust-analyzer
-            pkgs.trunk
+        craneLib = (crane.mkLib pkgs).overrideToolchain (_: rustToolchain);
+
+        unfilteredRoot = ./site;
+        src = pkgs.lib.fileset.toSource {
+          root = unfilteredRoot;
+          fileset = pkgs.lib.fileset.unions [
+            (craneLib.fileset.commonCargoSources unfilteredRoot)
+            (pkgs.lib.fileset.fileFilter (
+              file: pkgs.lib.any file.hasExt [ "html" "scss" "css" "js" "json" ]
+            ) unfilteredRoot)
+            (pkgs.lib.fileset.maybeMissing ./assets)
           ];
         };
-      });
+
+        commonArgs = {
+          inherit src;
+          strictDeps = true;
+          buildInputs = pkgs.lib.optionals pkgs.stdenv.isDarwin [
+            pkgs.libiconv
+          ];
+        };
+
+        nativeArgs = commonArgs // {
+          pname = "trunk-workspace-native";
+        };
+
+        cargoArtifacts = craneLib.buildDepsOnly nativeArgs;
+
+        wasmArgs = commonArgs // {
+          pname = "trunk-workspace-wasm";
+          cargoExtraArgs = "--package=bible";
+          CARGO_BUILD_TARGET = "wasm32-unknown-unknown";
+        };
+
+        cargoArtifactsWasm = craneLib.buildDepsOnly (wasmArgs // {
+          doCheck = false;
+        });
+
+        myClient = craneLib.buildTrunkPackage (
+          wasmArgs // {
+            pname = "trunk-workspace-client";
+            cargoArtifacts = cargoArtifactsWasm;
+
+            # Fix trunk caching issues in sandbox
+            preBuild = ''
+              mkdir -p target/trunk-cache
+            '';
+            TRUNK_CACHE_DIR = "./target/trunk-cache";
+            HOME = "./target/home";
+
+            postBuild = ''
+              mv ./dist ..
+              cd ..
+            '';
+
+            # Required wasm-bindgen-cli setup
+            wasm-bindgen-cli = pkgs.buildWasmBindgenCli rec {
+              src = pkgs.fetchCrate {
+                pname = "wasm-bindgen-cli";
+                version = "0.2.100";
+                hash = "sha256-3RJzK7mkYFrs7C/WkhW9Rr4LdP5ofb2FdYGz1P7Uxog=";
+              };
+
+              cargoDeps = pkgs.rustPlatform.fetchCargoVendor {
+                inherit src;
+                inherit (src) pname version;
+                hash = "sha256-qsO12332HSjWCVKtf1cUePWWb9IdYUmT+8OPj/XP2WE=";
+              };
+            };
+          }
+        );
+      in {
+        packages.default = myClient;
+
+        checks = {
+          inherit myClient;
+
+          clippy = craneLib.cargoClippy (commonArgs // {
+            inherit cargoArtifacts;
+            cargoClippyExtraArgs = "--all-targets -- --deny warnings";
+          });
+
+          fmt = craneLib.cargoFmt commonArgs;
+        };
+
+        devShells.default = craneLib.devShell {
+          checks = self.checks.${system};
+          packages = [ pkgs.trunk ];
+          shellHook = ''
+            export CLIENT_DIST=$PWD/client/dist
+          '';
+        };
+
+        apps.default = flake-utils.lib.mkApp {
+          name = "bible";
+          drv = myClient;
+        };
+      }
+    );
 }
