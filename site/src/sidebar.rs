25dba06f69571cc5fa061e8432565c966471d0b6
sem pruijs <sem@pruijs.net>
2025-07-01 12:37:08 +0200

    create working sidebar

--- site/src/sidebar.rs
+++ site/src/sidebar.rs
@@ -1,4 +1,4 @@
-use crate::Bible;
+use crate::types::*;
 use leptos::component;
 use leptos::prelude::*;
 use leptos::view;
@@ -7,18 +7,54 @@ use leptos_router::components::A;
 
 #[component]
 pub fn Sidebar<'a>(bible: &'a Bible) -> impl IntoView + 'a {
+    let (selected_book, set_selected_book) = signal(String::from("Genesis"));
+
     view! {
         <ul>
-            {bible.books.iter().flat_map(|b| b.chapters.iter().map(|c| {
-                let path = c.to_path();
-                let name = c.name.clone();
-                view! {
-                    <li>
-                        <A href={path}>{name}</A>
-                    </li>
-                }
-            }
-            ).collect_view()).collect_view()}
+        {bible.books.iter().map(|b| view! {
+            <BookView
+                book=b.clone()
+                selected_book=selected_book
+                set_selected_book=set_selected_book
+            />
+        }).collect_view()}
         </ul>
+    }
+}
+
+#[component]
+fn BookView(
+    book: Book,
+    selected_book: ReadSignal<String>,
+    set_selected_book: WriteSignal<String>,
+) -> impl IntoView {
+    let book_name = book.name.clone();
+    let book_name_2 = book.name.clone();
+
+    view! {
+        <li>
+            <button on:click=move |_| {
+                set_selected_book.update(|b| if *b == book_name {
+                    // When you want to collapse the chapters
+                    *b = String::new();
+                } else {
+                    *b = book_name.clone();
+                })
+            }>
+                {book_name.clone()}
+            </button>
+            <Show
+                when=move || selected_book.get() == book_name_2.clone()
+                fallback=|| view! { <></> }
+            >
+            <ul>
+            {book.chapters.iter().cloned().map(|c| view! {
+                <li>
+                    <A href=c.to_path() >{c.chapter}</A>
+                </li>
+            }).collect_view()}
+            </ul>
+            </Show>
+        </li>
     }
 }
