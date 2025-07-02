4affa3eea72a7ad657a0d7c29288672647f75df9
sem pruijs <sem@pruijs.net>
2025-07-02 22:50:01 +0200

    make faster

--- site/src/sidebar.rs
+++ site/src/sidebar.rs
@@ -6,7 +6,7 @@ use leptos::IntoView;
 use leptos_router::components::A;
 
 #[component]
-pub fn Sidebar<'a>(bible: &'a Bible) -> impl IntoView + 'a {
+pub fn Sidebar(bible: Bible) -> impl IntoView {
     let (selected_book, set_selected_book) = signal(String::from("Genesis"));
 
     view! {
@@ -31,26 +31,30 @@ fn BookView(
     selected_book: ReadSignal<String>,
     set_selected_book: WriteSignal<String>,
 ) -> impl IntoView {
-    let book_name = book.name.clone();
-    let book_name_2 = book.name.clone();
 
     view! {
         <li>
             <button 
                 class="w-full text-left px-3 py-2 rounded-md hover:bg-gray-200 transition-colors duration-150 font-medium text-gray-700"
-                on:click=move |_| {
-                    set_selected_book.update(|b| if *b == book_name {
-                        // When you want to collapse the chapters
-                        *b = String::new();
-                    } else {
-                        *b = book_name.clone();
-                    })
+                on:click={
+                    let book_name = book.name.clone();
+                    move |_| {
+                        set_selected_book.update(|b| if *b == book_name {
+                            // When you want to collapse the chapters
+                            *b = String::new();
+                        } else {
+                            *b = book_name.clone();
+                        })
+                    }
                 }
             >
-                {book_name.clone()}
+                {book.name.clone()}
             </button>
             <Show
-                when=move || selected_book.get() == book_name_2.clone()
+                when={
+                    let book_name = book.name.clone();
+                    move || selected_book.get() == book_name
+                }
                 fallback=|| view! { <></> }
             >
             <div class="ml-4 mt-2 grid grid-cols-5 gap-1">
