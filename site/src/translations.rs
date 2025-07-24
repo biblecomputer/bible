d3730fafa6ba31dcbe2acb8e0aa3537f8f2ab1e4
sem pruijs <sem@pruijs.net>
2025-07-24 15:25:14 +0200

    save petrus nl translation

--- site/src/translations.rs
+++ site/src/translations.rs
@@ -9,5 +9,14 @@ let translations = vec![
         year: 1618,
         languages: vec![Language::Dutch()],
         iagon: String::from("https://gw.iagon.com/api/v2/storage/shareable/link/Njg2ZDFjNDgwOGQ0M2UzNTUyNTdhYmRh:MTJjOTRlYTBmNzM2YWZiZDE2NzdkMzU3NzA3MjBmMTRmZGZkMWYzNWVkYWVlNTU1Y2RjYTA1NzYzZmE1YmEzNA"),
+    },
+    Translation {
+        name: String::from("Petrus Canisiusvertaling")
+        short_name: String::from("sv"),
+        description: String::from("De Statenvertaling is een Nederlandse Bijbelvertaling uit 1637, vervaardigd in opdracht van de Staten-Generaal. Zij baseerden zich nauwgezet op de oorspronkelijke Hebreeuwse en Griekse grondteksten. Deze vertaling wordt gekenmerkt door haar plechtige, eerbiedige taal en heeft eeuwenlang grote invloed gehad op het protestantse geloofsleven in Nederland."),
+        wikipedia: String::from("https://nl.wikipedia.org/wiki/Petrus_Canisiusvertaling"),
+        year: 1939,
+        languages: vec![Language::Dutch()],
+        iagon: String::from("https://gw.iagon.com/api/v2/storage/shareable/link/Njg4MTA1YThlMTNjNGI2YzhhMjM0YmNm:MGI0OWJkYzcyMDNkZWYxNzRkODQ0NTA1MTY2ZDVmOGI4MjI0ZjE2MzFiYmM4MDI2ZGU5MTZmOWJiNzVjZDYxMw"),
     }
-]
+];
