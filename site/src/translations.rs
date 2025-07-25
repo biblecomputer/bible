8ef71f5541dd006fb9ec25af780cb7a82580da93
sem pruijs <sem@pruijs.net>
2025-07-26 00:53:18 +0200

    add king james version

--- site/src/translations.rs
+++ site/src/translations.rs
@@ -19,6 +19,15 @@ pub fn get_translations() -> Vec<Translation> {
             release_year: 1939,
             languages: vec![Language::Dutch],
             iagon: String::from("https://gw.iagon.com/api/v2/storage/shareable/link/Njg4MTA1YThlMTNjNGI2YzhhMjM0YmNm:MGI0OWJkYzcyMDNkZWYxNzRkODQ0NTA1MTY2ZDVmOGI4MjI0ZjE2MzFiYmM4MDI2ZGU5MTZmOWJiNzVjZDYxMw"),
+        },
+        Translation {
+            name: String::from("King james version"),
+            short_name: String::from("kjv"),
+            description: String::from("The King James Version is an English translation of the Bible published in 1611, commissioned by King James I of England. It was created by 47 scholars and is known for its literary excellence and enduring influence on English literature and Protestant Christianity. The KJV uses formal equivalence translation and features majestic, archaic English that has shaped religious language for centuries."),
+            wikipedia: String::from("https://nl.wikipedia.org/wiki/King_James_Version"),
+            release_year: 1611,
+            languages: vec![Language::English],
+            iagon: String::from("https://gw.iagon.com/api/v2/storage/shareable/link/Njg4M2Y3NmRlMTNjNGI2YzhhMmQyM2Ji:ZmIwZjczMTNkZGZkNjFhYjkwMjYzNWE0NGUwNzEzMGU4YThjYTZjNWZmOTdiZWJkNTg0ZDFhODVlNjBjMTZhNw"),
         }
     ]
 }
