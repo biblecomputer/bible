9d963aa64c35f4f07e8a1dc0d28b892e69e74639
sem pruijs <sem@pruijs.net>
2025-08-06 16:12:54 +0200

    add themes sidebar

--- site/src/components/mod.rs
+++ site/src/components/mod.rs
@@ -1,11 +1,13 @@
 pub mod command_palette;
 pub mod sidebar;
 pub mod theme_switcher;
+pub mod theme_sidebar;
 pub mod translation_switcher;
 pub mod cross_references_sidebar;
 
 pub use command_palette::*;
 pub use sidebar::*;
 pub use theme_switcher::*;
+pub use theme_sidebar::*;
 pub use cross_references_sidebar::*;
 // pub use translation_switcher::*;
