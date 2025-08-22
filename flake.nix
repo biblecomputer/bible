4ab169305fcebb020700fe3758b6ec972ffa6a8f
sem pruijs <sem@pruijs.net>
2025-08-22 12:37:48 +0200

    write bible-verify derivation

--- flake.nix
+++ flake.nix
@@ -110,7 +110,24 @@
           }
         );
       in {
-        packages.default = myClient;
+        packages = {
+          default = myClient;
+          bible-verify = craneLib.buildPackage {
+            src = pkgs.lib.fileset.toSource {
+              root = ./bible-verify;
+              fileset = pkgs.lib.fileset.unions [
+                (craneLib.fileset.commonCargoSources ./bible-verify)
+                (pkgs.lib.fileset.maybeMissing ./bible-verify/kjv.json)
+              ];
+            };
+            pname = "bible-verify";
+            version = "0.1.0";
+            strictDeps = true;
+            buildInputs = pkgs.lib.optionals pkgs.stdenv.isDarwin [
+              pkgs.libiconv
+            ];
+          };
+        };
 
         checks = {
           inherit myClient;
